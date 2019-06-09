d9b9176fcb32dfa75730cd6ba62a505cc4b3687e
kennyhe <kennyhe@tcl.com>
2019-06-09 14:46:20 +0800

    Initial commit

---
+++ app/src/main/java/com/hk/myview/Ball.java
@@ -0,0 +1,17 @@
+package com.hk.myview;
+
+/**
+ * Created by hk on 2019/6/9.
+ */
+public class Ball {
+
+    public int color;
+    public float x;
+    public float y;
+    public float r;
+
+    public float vX;
+    public float vY;
+    public float aX;
+    public float aY;
+}
